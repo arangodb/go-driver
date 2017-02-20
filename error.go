2f32495137332a38751fe9bc218b3c2b23d4a50e
Ewout Prangsma <ewout@prangsma.net>
2017-02-20 17:17:43 +0100

    Creating interface

---
+++ error.go
@@ -0,0 +1,52 @@
+//
+// DISCLAIMER
+//
+// Copyright 2017 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+// Author Ewout Prangsma
+//
+
+package driver
+
+// ArangoError is a Go error with arangodb specific error information.
+type ArangoError struct {
+	HasError     bool   `json:"error"`
+	Code         int    `json:"code"`
+	ErrorNum     int    `json:"errorNum"`
+	ErrorMessage string `json:"errorMessage"`
+}
+
+// Error returns the error message of an ArangoError.
+func (ae ArangoError) Error() string {
+	return ae.ErrorMessage
+}
+
+// IsArangoErrorWithCode returns true when the given error is an ArangoError and its Code field is equal to the given code.
+func IsArangoErrorWithCode(err error, code int) bool {
+	ae, ok := err.(ArangoError)
+	return ok && ae.Code == code
+}
+
+// IsInvalidRequest return true if the given error is an ArangoError with code 400, indicating an invalid request.
+func IsInvalidRequest(err error) bool {
+	return IsArangoErrorWithCode(err, 400)
+}
+
+// IsNotFound return true if the given error is an ArangoError with code 404, indicating a object not found.
+func IsNotFound(err error) bool {
+	return IsArangoErrorWithCode(err, 404)
+}
