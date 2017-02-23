4da306c7acdf4de8ac9063caceeb699b832d6108
Ewout Prangsma <ewout@prangsma.net>
2017-02-23 16:23:29 +0100

    More collection tests

--- test/collection_test.go
+++ test/collection_test.go
@@ -37,3 +37,44 @@ func TestCreateCollection(t *testing.T) {
 		t.Errorf("CollectionExists('%s') return false, expected true", name)
 	}
 }
+
+// TestRemoveCollection creates a collection and then removes it.
+func TestRemoveCollection(t *testing.T) {
+	c := createClientFromEnv(t, true)
+	db := ensureDatabase(nil, c, "collection_test", nil, t)
+	name := "test_remove_collection"
+	col, err := db.CreateCollection(nil, name, nil)
+	if err != nil {
+		t.Fatalf("Failed to create collection '%s': %s", name, describe(err))
+	}
+	// Collection must exist now
+	if found, err := db.CollectionExists(nil, name); err != nil {
+		t.Errorf("CollectionExists('%s') failed: %s", name, describe(err))
+	} else if !found {
+		t.Errorf("CollectionExists('%s') return false, expected true", name)
+	}
+	// Now remove it
+	if err := col.Remove(nil); err != nil {
+		t.Fatalf("Failed to remove collection '%s': %s", name, describe(err))
+	}
+	// Collection must not exist now
+	if found, err := db.CollectionExists(nil, name); err != nil {
+		t.Errorf("CollectionExists('%s') failed: %s", name, describe(err))
+	} else if found {
+		t.Errorf("CollectionExists('%s') return true, expected false", name)
+	}
+}
+
+// TestCollectionName creates a collection and checks its name
+func TestCollectionName(t *testing.T) {
+	c := createClientFromEnv(t, true)
+	db := ensureDatabase(nil, c, "collection_test", nil, t)
+	name := "test_remove_collection"
+	col, err := db.CreateCollection(nil, name, nil)
+	if err != nil {
+		t.Fatalf("Failed to create collection '%s': %s", name, describe(err))
+	}
+	if col.Name() != name {
+		t.Errorf("Collection.Name() is wrong, got '%s', expected '%s'", col.Name(), name)
+	}
+}
