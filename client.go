c7874c2949fa85771514a1c47e6b8e21860d1146
Ewout Prangsma <ewout@prangsma.net>
2017-03-03 08:50:33 +0100

    Extracted ClientDatabases interface for readability

--- client.go
+++ client.go
@@ -30,22 +30,8 @@ type Client interface {
 	// Use WithDetails to configure a context that will include additional details in the return VersionInfo.
 	Version(ctx context.Context) (VersionInfo, error)
 
-	// Database opens a connection to an existing database.
-	// If no database with given name exists, an NotFoundError is returned.
-	Database(ctx context.Context, name string) (Database, error)
-
-	// DatabaseExists returns true if a database with given name exists.
-	DatabaseExists(ctx context.Context, name string) (bool, error)
-
-	// Databases returns a list of all databases found by the client.
-	Databases(ctx context.Context) ([]Database, error)
-
-	// AccessibleDatabases returns a list of all databases that can be accessed by the authenticated user.
-	AccessibleDatabases(ctx context.Context) ([]Database, error)
-
-	// CreateDatabase creates a new database with given name and opens a connection to it.
-	// If the a database with given name already exists, a DuplicateError is returned.
-	CreateDatabase(ctx context.Context, name string, options *CreateDatabaseOptions) (Database, error)
+	// Database functions
+	ClientDatabases
 }
 
 // ClientConfig contains all settings needed to create a client.
@@ -68,24 +54,4 @@ type VersionInfo struct {
 	License string `json:"license,omitempty"`
 	// Optional additional details. This is returned only if the context is configured using WithDetails.
 	Details map[string]interface{} `json:"details,omitempty"`
-}
-
-// CreateDatabaseOptions contains options that customize the creating of a database.
-type CreateDatabaseOptions struct {
-	// List of users to initially create for the new database. User information will not be changed for users that already exist.
-	// If users is not specified or does not contain any users, a default user root will be created with an empty string password.
-	// This ensures that the new database will be accessible after it is created.
-	Users []CreateDatabaseUserOptions `json:"users,omitempty"`
-}
-
-// CreateDatabaseUserOptions contains options for creating a single user for a database.
-type CreateDatabaseUserOptions struct {
-	// Loginname of the user to be created
-	UserName string `json:"user,omitempty"`
-	// The user password as a string. If not specified, it will default to an empty string.
-	Password string `json:"passwd,omitempty"`
-	// A flag indicating whether the user account should be activated or not. The default value is true. If set to false, the user won't be able to log into the database.
-	Active *bool `json:"active,omitempty"`
-	// A JSON object with extra user information. The data contained in extra will be stored for the user but not be interpreted further by ArangoDB.
-	Extra interface{} `json:"extra,omitempty"`
 }
