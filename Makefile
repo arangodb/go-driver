5fb8b5f09433897318f4a712a6e1806361fa0647
Ewout Prangsma <ewout@prangsma.net>
2017-03-13 11:01:44 +0100

    Added cluster-with-authentication tests

--- Makefile
+++ Makefile
@@ -37,8 +37,9 @@ $(GOBUILDDIR):
 
 DBCONTAINER := $(PROJECT)-test-db
 
-run-tests: run-tests-http run-tests-single-with-auth run-tests-single-no-auth
+run-tests: run-tests-http run-tests-single run-test-cluster
 
+# Tests of HTTP package 
 run-tests-http: $(GOBUILDDIR)
 	@docker run \
 		--rm \
@@ -47,6 +48,9 @@ run-tests-http: $(GOBUILDDIR)
 		-w /usr/code/ \
 		golang:$(GOVERSION) \
 		go test $(TESTOPTIONS) $(REPOPATH)/http
+
+# Single server tests 
+run-tests-single: run-tests-single-with-auth run-tests-single-no-auth
 
 run-tests-single-no-auth: $(GOBUILDDIR)
 	@echo "Single server, no authentication"
@@ -83,6 +87,9 @@ run-tests-single-with-auth: $(GOBUILDDIR)
 		go test -tags auth $(TESTOPTIONS) $(REPOPATH)/test
 	@-docker rm -f -v $(DBCONTAINER) &> /dev/null
 
+# Cluster mode tests
+run-tests-cluster: run-tests-cluster-no-auth run-tests-cluster-with-auth
+
 run-tests-cluster-no-auth: $(GOBUILDDIR)
 	@echo "Cluster server, no authentication"
 	@PROJECT=$(PROJECT) ARANGODB=$(ARANGODB) $(ROOTDIR)/test/cluster.sh start
@@ -95,6 +102,21 @@ run-tests-cluster-no-auth: $(GOBUILDDIR)
 		-w /usr/code/ \
 		golang:$(GOVERSION) \
 		go test $(TESTOPTIONS) $(REPOPATH)/test
+	@PROJECT=$(PROJECT) ARANGODB=$(ARANGODB) $(ROOTDIR)/test/cluster.sh cleanup
+
+run-tests-cluster-with-auth: $(GOBUILDDIR)
+	@echo "Cluster server, with authentication"
+	@PROJECT=$(PROJECT) ARANGODB=$(ARANGODB) TMPDIR=${GOBUILDDIR} JWTSECRET=testing $(ROOTDIR)/test/cluster.sh start
+	docker run \
+		--rm \
+		--net=host \
+		-v $(ROOTDIR):/usr/code \
+		-e GOPATH=/usr/code/.gobuild \
+		-e TEST_ENDPOINTS=http://localhost:7002 \
+		-e TEST_AUTHENTICATION=basic:root: \
+		-w /usr/code/ \
+		golang:$(GOVERSION) \
+		go test  -tags auth $(TESTOPTIONS) $(REPOPATH)/test
 	@PROJECT=$(PROJECT) ARANGODB=$(ARANGODB) $(ROOTDIR)/test/cluster.sh cleanup
 
 run-tests-cluster-cleanup:
--- test/cluster.sh
+++ test/cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-if [ -e "${PROJECT}" ]; then 
+if [ -z "$PROJECT" ]; then 
     echo "PROJECT environment variable must be set"
     exit 1 
 fi
@@ -12,13 +12,15 @@ STARTERCONTAINER1=${PROJECT}-test-s1
 STARTERCONTAINER2=${PROJECT}-test-s2
 STARTERCONTAINER3=${PROJECT}-test-s3
 CMD=$1
+DOCKERARGS=
+STARTERARGS=
 
 # Cleanup
 docker rm -f -v $(docker ps -a | grep ${PROJECT}-test | awk '{print $1}') &> /dev/null
 docker volume rm -f ${STARTERVOLUME1} ${STARTERVOLUME2} ${STARTERVOLUME3} &> /dev/null
 
 if [ "$CMD" == "start" ]; then
-    if [ -e "${ARANGODB}" ]; then 
+    if [ -z "$ARANGODB" ]; then 
         echo "ARANGODB environment variable must be set"
         exit 1 
     fi
@@ -28,14 +30,26 @@ if [ "$CMD" == "start" ]; then
     docker volume create ${STARTERVOLUME2} &> /dev/null
     docker volume create ${STARTERVOLUME3} &> /dev/null
 
+    # Setup args 
+    if [ -n "$JWTSECRET" ]; then 
+        if [ -z "$TMPDIR" ]; then 
+            echo "TMPDIR environment variable must be set"
+            exit 1 
+        fi
+        JWTSECRETFILE="$TMPDIR/$PROJECT-jwtsecret"
+        echo "$JWTSECRET" > ${JWTSECRETFILE}
+        DOCKERARGS="$DOCKERARGS -v $JWTSECRETFILE:/jwtsecret:ro"
+        STARTERARGS="$STARTERARGS --jwtSecretFile=/jwtsecret"
+    fi 
+
     # Start starters 
     docker run -d --name=${STARTERCONTAINER1} --net=host \
-        -v ${STARTERVOLUME1}:/data -v /var/run/docker.sock:/var/run/docker.sock arangodb/arangodb-starter \
-        --dockerContainer=${STARTERCONTAINER1} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB}
+        -v ${STARTERVOLUME1}:/data -v /var/run/docker.sock:/var/run/docker.sock $DOCKERARGS arangodb/arangodb-starter \
+        --dockerContainer=${STARTERCONTAINER1} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB} $STARTERARGS
     docker run -d --name=${STARTERCONTAINER2} --net=host \
-        -v ${STARTERVOLUME2}:/data -v /var/run/docker.sock:/var/run/docker.sock arangodb/arangodb-starter \
-        --dockerContainer=${STARTERCONTAINER2} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB} --join=127.0.0.1
+        -v ${STARTERVOLUME2}:/data -v /var/run/docker.sock:/var/run/docker.sock $DOCKERARGS arangodb/arangodb-starter \
+        --dockerContainer=${STARTERCONTAINER2} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB} $STARTERARGS --join=127.0.0.1
     docker run -d --name=${STARTERCONTAINER3} --net=host \
-        -v ${STARTERVOLUME3}:/data -v /var/run/docker.sock:/var/run/docker.sock arangodb/arangodb-starter \
-        --dockerContainer=${STARTERCONTAINER3} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB} --join=127.0.0.1
+        -v ${STARTERVOLUME3}:/data -v /var/run/docker.sock:/var/run/docker.sock $DOCKERARGS arangodb/arangodb-starter \
+        --dockerContainer=${STARTERCONTAINER3} --dockerNetHost --masterPort=7000 --ownAddress=127.0.0.1 --docker=${ARANGODB} $STARTERARGS --join=127.0.0.1
 fi
