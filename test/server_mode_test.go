5630b85d9ccf0a93f754dd406387c7370989cce4
Ewout Prangsma <ewout@prangsma.net>
2017-11-27 10:17:46 +0100

    Skip test of pre-3.3 versions

--- test/server_mode_test.go
+++ test/server_mode_test.go
@@ -34,42 +34,51 @@ func TestServerMode(t *testing.T) {
 	c := createClientFromEnv(t, true)
 	ctx := context.Background()
 
-	// Create simple collection
-	db := ensureDatabase(ctx, c, "_system", nil, t)
-	colName := "server_mode_test1"
-	col := ensureCollection(ctx, db, colName, nil, t)
-
-	// Initial server mode must be default
-	if mode, err := c.ServerMode(ctx); err != nil {
-		t.Fatalf("ServerMode failed: %s", describe(err))
-	} else if mode != driver.ServerModeDefault {
-		t.Errorf("ServerMode returned '%s', but expected '%s'", mode, driver.ServerModeDefault)
+	version, err := c.Version(nil)
+	if err != nil {
+		t.Fatalf("Version failed: %s", describe(err))
 	}
+	isv33p := version.Version.CompareTo("3.3") >= 0
+	if !isv33p {
+		t.Skip("This test requires version 3.3")
+	} else {
+		// Create simple collection
+		db := ensureDatabase(ctx, c, "_system", nil, t)
+		colName := "server_mode_test1"
+		col := ensureCollection(ctx, db, colName, nil, t)
 
-	// Change server mode to readonly.
-	if err := c.SetServerMode(ctx, driver.ServerModeReadOnly); err != nil {
-		t.Fatalf("SetServerMode failed: %s", describe(err))
-	}
+		// Initial server mode must be default
+		if mode, err := c.ServerMode(ctx); err != nil {
+			t.Fatalf("ServerMode failed: %s", describe(err))
+		} else if mode != driver.ServerModeDefault {
+			t.Errorf("ServerMode returned '%s', but expected '%s'", mode, driver.ServerModeDefault)
+		}
 
-	// Try to drop collection now (it must fail)
-	if err := col.Remove(ctx); !driver.IsForbidden(err) {
-		t.Fatalf("Collection remove should have return ForbiddenError, got error %s", describe(err))
-	}
+		// Change server mode to readonly.
+		if err := c.SetServerMode(ctx, driver.ServerModeReadOnly); err != nil {
+			t.Fatalf("SetServerMode failed: %s", describe(err))
+		}
 
-	// Check server mode, must be readonly
-	if mode, err := c.ServerMode(ctx); err != nil {
-		t.Fatalf("ServerMode failed: %s", describe(err))
-	} else if mode != driver.ServerModeReadOnly {
-		t.Errorf("ServerMode returned '%s', but expected '%s'", mode, driver.ServerModeReadOnly)
-	}
+		// Try to drop collection now (it must fail)
+		if err := col.Remove(ctx); !driver.IsForbidden(err) {
+			t.Fatalf("Collection remove should have return ForbiddenError, got error %s", describe(err))
+		}
 
-	// Change server mode back to default.
-	if err := c.SetServerMode(ctx, driver.ServerModeDefault); err != nil {
-		t.Fatalf("SetServerMode failed: %s", describe(err))
-	}
+		// Check server mode, must be readonly
+		if mode, err := c.ServerMode(ctx); err != nil {
+			t.Fatalf("ServerMode failed: %s", describe(err))
+		} else if mode != driver.ServerModeReadOnly {
+			t.Errorf("ServerMode returned '%s', but expected '%s'", mode, driver.ServerModeReadOnly)
+		}
 
-	// Try to drop collection now (it must succeed)
-	if err := col.Remove(ctx); err != nil {
-		t.Fatalf("Collection remove failed: %s", describe(err))
+		// Change server mode back to default.
+		if err := c.SetServerMode(ctx, driver.ServerModeDefault); err != nil {
+			t.Fatalf("SetServerMode failed: %s", describe(err))
+		}
+
+		// Try to drop collection now (it must succeed)
+		if err := col.Remove(ctx); err != nil {
+			t.Fatalf("Collection remove failed: %s", describe(err))
+		}
 	}
 }
