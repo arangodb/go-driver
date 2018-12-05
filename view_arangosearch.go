613274a33fad87590ab2ac343478f3ceccb3bd54
Lars Maier <lars@arangodb.com>
2018-12-05 09:32:21 +0100

    ArangoView API Changes (#161)
    * API changes.
    * Do more magic with typing for ConsolidationPolicies.
    * Keep it simple and stupid - single structure for ArangoSearchProperties.

--- view_arangosearch.go
+++ view_arangosearch.go
@@ -51,7 +51,7 @@ type ArangoSearchViewProperties struct {
 	// For the case where the consolidation policies rarely merge segments
 	// (i.e. few inserts/deletes), a higher value will impact performance
 	// without any added benefits.
-	CleanupIntervalStep int64 `json:"cleanupIntervalStep,omitempty"`
+	CleanupIntervalStep *int64 `json:"cleanupIntervalStep,omitempty"`
 	// ConsolidationInterval specifies the minimum number of milliseconds that must be waited
 	// between committing index data changes and making them visible to queries.
 	// Defaults to 60000.
@@ -62,33 +62,73 @@ type ArangoSearchViewProperties struct {
 	// For the case where there are a few inserts/updates, a higher value will
 	// impact performance and waste disk space for each commit call without
 	// any added benefits.
-	ConsolidationInterval int64 `json:"consolidationIntervalMsec,omitempty"`
+	ConsolidationInterval *int64 `json:"consolidationIntervalMsec,omitempty"`
 	// ConsolidationPolicy specifies thresholds for consolidation.
-	ConsolidationPolicy *ArangoSearchConsolidationPolicy `json:"consolidationPolicy,omitempty"`
+	ConsolidationPolicy ArangoSearchConsolidationPolicy `json:"consolidationPolicy,omitempty"`
 
-	/*
-		// Locale specifies the default locale used for queries on analyzed string values.
-		// Defaults to "C". TODO What is that?
-		Locale Locale `json:"locale,omitempty"`
-	*/
+	// WriteBufferIdel specifies the maximum number of writers (segments) cached in the pool.
+	// 0 value turns off caching, default value is 64.
+	WriteBufferIdel *int64 `json:"writebufferIdle,omitempty"`
+
+	// WriteBufferActive specifies the maximum number of concurrent active writers (segments) performs (a transaction).
+	// Other writers (segments) are wait till current active writers (segments) finish.
+	// 0 value turns off this limit and used by default.
+	WriteBufferActive *int64 `json:"writebufferActive,omitempty"`
+
+	// WriteBufferSizeMax specifies maximum memory byte size per writer (segment) before a writer (segment) flush is triggered.
+	// 0 value turns off this limit fon any writer (buffer) and will be flushed only after a period defined for special thread during ArangoDB server startup.
+	// 0 value should be used with carefully due to high potential memory consumption.
+	WriteBufferSizeMax *int64 `json:"writebufferSizeMax,omitempty"`
+
 	// Links contains the properties for how individual collections
 	// are indexed in thie view.
 	// The key of the map are collection names.
 	Links ArangoSearchLinks `json:"links,omitempty"`
 }
 
-// Locale is a strongly typed specifier of a locale.
-// TODO specify semantics.
-type Locale string
+// ArangoSearchConsolidationPolicyType strings for consolidation types
+type ArangoSearchConsolidationPolicyType string
+
+const (
+	// ArangoSearchConsolidationPolicyTypeTier consolidate based on segment byte size and live document count as dictated by the customization attributes.
+	ArangoSearchConsolidationPolicyTypeTier ArangoSearchConsolidationPolicyType = "tier"
+	// ArangoSearchConsolidationPolicyTypeBytesAccum consolidate if and only if ({threshold} range [0.0, 1.0])
+	// {threshold} > (segment_bytes + sum_of_merge_candidate_segment_bytes) / all_segment_bytes,
+	// i.e. the sum of all candidate segment's byte size is less than the total segment byte size multiplied by the {threshold}.
+	ArangoSearchConsolidationPolicyTypeBytesAccum ArangoSearchConsolidationPolicyType = "bytes_accum"
+)
 
 // ArangoSearchConsolidationPolicy holds threshold values specifying when to
 // consolidate view data.
 // Semantics of the values depend on where they are used.
 type ArangoSearchConsolidationPolicy struct {
-	// Threshold is a percentage (0..1)
-	Threshold float64 `json:"threshold,omitempty"`
-	// SegmentThreshold is an absolute value.
-	SegmentThreshold int64 `json:"segmentThreshold,omitempty"`
+	// Type returns the type of the ConsolidationPolicy. This interface can then be casted to the corresponding ArangoSearchConsolidationPolicy* struct.
+	Type ArangoSearchConsolidationPolicyType
+
+	ArangoSearchConsolidationPolicyBytesAccum
+	ArangoSearchConsolidationPolicyTier
+}
+
+// ArangoSearchConsolidationPolicyBytesAccum contains fields used for ArangoSearchConsolidationPolicyTypeBytesAccum
+type ArangoSearchConsolidationPolicyBytesAccum struct {
+	// Threshold, see ArangoSearchConsolidationTypeBytesAccum
+	Threshold *float64 `json:"threshold,omitempty"`
+}
+
+// ArangoSearchConsolidationPolicyTier contains fields used for ArangoSearchConsolidationPolicyTypeTier
+type ArangoSearchConsolidationPolicyTier struct {
+	// MinSegments specifies the minimum number of segments that will be evaluated as candidates for consolidation.
+	MinSegments *int64 `json:"minSegments,omitempty"`
+	// MaxSegments specifies the maximum number of segments that will be evaluated as candidates for consolidation.
+	MaxSegments *int64 `json:"maxSegments,omitempty"`
+	// SegmentsBytesMax specifies the maxinum allowed size of all consolidated segments in bytes.
+	SegmentsBytesMax *int64 `json:"segmentsBytesMax,omitempty"`
+	// SegmentsBytesFloor defines the value (in bytes) to treat all smaller segments as equal for consolidation selection.
+	SegmentsBytesFloor *int64 `json:"segmentsBytesFloor,omitempty"`
+	// Lookahead specifies the number of additionally searched tiers except initially chosen candidated based on min_segments,
+	// max_segments, segments_bytes_max, segments_bytes_floor with respect to defined values.
+	// Default value falls to integer_traits<size_t>::const_max (in C++ source code).
+	Lookahead *int64 `json:"lookahead,omitempty"`
 }
 
 // ArangoSearchLinks is a strongly typed map containing links between a
