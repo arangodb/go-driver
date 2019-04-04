f4c4c3bbfffd32f35d1f3a3ad429f4b22a8d1c04
lamai93 <lars@arangodb.com>
2019-04-04 12:17:27 +0200

    Added method to create jwt tokens with allowed_path.

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -54,3 +54,29 @@ func CreateArangodJwtAuthorizationHeader(jwtSecret, serverID string) (string, er
 
 	return "bearer " + signedToken, nil
 }
+
+// CreateArangodJwtAuthorizationHeaderAllowedPaths calculates a JWT authorization header, for authorization
+// of a request to an arangod server, based on the given secret.
+// If the secret is empty, nothing is done.
+// Use the result of this function as input for driver.RawAuthentication.
+// Additionally allowed paths can be specified
+func CreateArangodJwtAuthorizationHeaderAllowedPaths(jwtSecret, serverID string, paths []string) (string, error) {
+	if jwtSecret == "" || serverID == "" {
+		return "", nil
+	}
+	// Create a new token object, specifying signing method and the claims
+	// you would like it to contain.
+	token := jg.NewWithClaims(jg.SigningMethodHS256, jg.MapClaims{
+		"iss":           issArangod,
+		"server_id":     serverID,
+		"allowed_paths": paths,
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	signedToken, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return "", driver.WithStack(err)
+	}
+
+	return "bearer " + signedToken, nil
+}
