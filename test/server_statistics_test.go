404c38b1d905ad35e7bb3f6b9171c82e2d0cc2b7
Max Neunhoeffer <max@arangodb.com>
2019-10-24 17:08:10 +0200

    Add a test for traffic accounting when forwarding.

--- test/server_statistics_test.go
+++ test/server_statistics_test.go
@@ -42,6 +42,22 @@ func checkEnabled(t *testing.T, c driver.Client, ctx context.Context) {
 	}
 }
 
+// doSomeWrites does some writes
+func doSomeWrites(t *testing.T, ctx context.Context, c driver.Client) {
+	db := ensureDatabase(ctx, c, "statistics_test", nil, t)
+	col := ensureCollection(ctx, db, "statistics_test", nil, t)
+	doc := UserDoc{
+	  "Max",
+		50,
+  }
+	for i := 0; i < 1000; i++ {
+		_, err := col.CreateDocument(ctx, doc)
+		if err != nil {
+		  t.Fatalf("Failed to create new document: %s", describe(err))
+		}
+	}
+}
+
 // TestServerStatisticsWorks tests if Client.Statistics works at all
 func TestServerStatisticsWorks(t *testing.T) {
 	c := createClientFromEnv(t, true)
@@ -60,6 +76,95 @@ func TestServerStatisticsWorks(t *testing.T) {
   //t.Logf("Statistics: %s", string(b))
 }
 
+type source int
+
+const (
+	user source = iota
+	all source  = iota
+)
+
+type limits struct {
+	Received  float64
+  Sent      float64
+	RecCount  int64
+	SentCount int64
+}
+
+
+// checkTrafficAtLeast compares stats before and after some operation and
+// checks that at least some amount of traffic has happened.
+func checkTrafficAtLeast(t *testing.T, statsBefore *driver.ServerStatistics, statsAfter *driver.ServerStatistics, which source, lim *limits) {
+	var before *driver.ClientStats
+	var after *driver.ClientStats
+	var name string
+	if which == user {
+		before = &statsBefore.ClientUser
+		after = &statsAfter.ClientUser
+		name = "ClientUser"
+	} else {
+		before = &statsBefore.Client
+    after = &statsAfter.Client
+		name = "Client"
+	}
+	diff := after.BytesReceived.Sum - before.BytesReceived.Sum
+	if diff < lim.Received {
+		t.Errorf("Difference in %s.BytesReceived.Sum is too small (< %f): %f",
+		         name, lim.Received, diff)
+	}
+	diff = after.BytesSent.Sum - before.BytesSent.Sum
+	if diff < lim.Sent {
+		t.Errorf("Difference in %s.BytesSent.Sum is too small (< %f): %f",
+		         name, lim.Sent, diff)
+	}
+	intDiff := after.BytesReceived.Count - before.BytesReceived.Count
+	if intDiff < lim.RecCount {
+		t.Errorf("Difference in %s.BytesReceived.Count is too small (< %d): %d",
+		         name, lim.RecCount, intDiff)
+	}
+	intDiff = after.BytesSent.Count - before.BytesSent.Count
+	if intDiff < lim.SentCount {
+		t.Errorf("Difference in %s.BytesSent.Count is too small (< %d): %d",
+		         name, lim.SentCount, intDiff)
+	}
+}
+
+// checkTrafficAtMost compares stats before and after some operation and
+// checks that at most some amount of traffic has happened.
+func checkTrafficAtMost(t *testing.T, statsBefore *driver.ServerStatistics, statsAfter *driver.ServerStatistics, which source, lim *limits) {
+	var before *driver.ClientStats
+	var after *driver.ClientStats
+	var name string
+	if which == user {
+		before = &statsBefore.ClientUser
+		after = &statsAfter.ClientUser
+		name = "ClientUser"
+	} else {
+		before = &statsBefore.Client
+    after = &statsAfter.Client
+		name = "Client"
+	}
+	diff := after.BytesReceived.Sum - before.BytesReceived.Sum
+	if diff > lim.Received {
+		t.Errorf("Difference in %s.BytesReceived.Sum is too large (> %f): %f",
+		         name, lim.Received, diff)
+	}
+	diff = after.BytesSent.Sum - before.BytesSent.Sum
+	if diff > lim.Sent {
+		t.Errorf("Difference in %s.BytesSent.Sum is too large (> %f): %f",
+		         name, lim.Sent, diff)
+	}
+	intDiff := after.BytesReceived.Count - before.BytesReceived.Count
+	if intDiff > lim.RecCount {
+		t.Errorf("Difference in %s.BytesReceived.Count is too large (> %d): %d",
+		         name, lim.RecCount, intDiff)
+	}
+	intDiff = after.BytesSent.Count - before.BytesSent.Count
+	if intDiff > lim.SentCount {
+		t.Errorf("Difference in %s.BytesSent.Count is too large (> %d): %d",
+		         name, lim.SentCount, intDiff)
+	}
+}
+
 // TestServerStatisticsTraffic tests if Client.Statistics increase
 // with traffic in the correct way
 func TestServerStatisticsTraffic(t *testing.T) {
@@ -73,18 +178,7 @@ func TestServerStatisticsTraffic(t *testing.T) {
 		t.Fatalf("Error in statistics call: %s", describe(err))
 	}
 
-	db := ensureDatabase(nil, c, "statistics_test", nil, t)
-	col := ensureCollection(nil, db, "statistics_test", nil, t)
-	doc := UserDoc{
-	  "Max",
-		50,
-  }
-	for i := 0; i < 1000; i++ {
-		_, err := col.CreateDocument(nil, doc)
-		if err != nil {
-		  t.Fatalf("Failed to create new document: %s", describe(err))
-		}
-	}
+	doSomeWrites(t, nil, c)
 
 	time.Sleep(time.Second)  // Wait until statistics updated
 
@@ -93,29 +187,9 @@ func TestServerStatisticsTraffic(t *testing.T) {
 		t.Fatalf("Error in statistics call: %s", describe(err))
 	}
 
-	//b, _ := json.Marshal(statsBefore)
-	//t.Logf("statsBefore: %s", string(b))
-	//b, _ = json.Marshal(statsAfter)
-	//t.Logf("statsAfter: %s", string(b))
-
-  var diff float64
-	diff = statsAfter.Client.BytesReceived.Sum - statsBefore.Client.BytesReceived.Sum
-  if diff < 40000.0 {
-		t.Errorf("Difference in Client.BytesReceived.Sum is too small (< 40000.0): %f", diff)
-	}
-	diff = statsAfter.Client.BytesSent.Sum - statsBefore.Client.BytesSent.Sum
-  if diff < 100000.0 {
-		t.Errorf("Difference in Client.BytesSent.Sum is too small (< 100000.0): %f", diff)
-	}
-	var intdiff int64
-	intdiff = statsAfter.Client.BytesReceived.Count - statsBefore.Client.BytesReceived.Count
-  if intdiff < 1000 {
-		t.Errorf("Difference in Client.BytesReceived.Count is too small (< 1000): %d", intdiff)
-	}
-	intdiff = statsAfter.Client.BytesSent.Count - statsBefore.Client.BytesSent.Count
-  if intdiff < 1000 {
-		t.Errorf("Difference in Client.BytesSent.Count is too small (< 1000): %d", intdiff)
-	}
+	checkTrafficAtLeast(t, &statsBefore, &statsAfter, all,
+											&limits{Sent: 100000.0, Received: 40000.0,
+															SentCount: 1000, RecCount: 1000});
 
 	// Now check if user only stats are there and see if they should have increased:
   if statsBefore.ClientUser.BytesReceived.Counts != nil {
@@ -123,47 +197,155 @@ func TestServerStatisticsTraffic(t *testing.T) {
 		auth := os.Getenv("TEST_AUTHENTICATION")
 		if auth == "super:testing" {
 			t.Logf("Authentication %s is jwt superuser, expecting no user traffic...", auth)
-	    // Traffic is superuser, so nothing should be counted in ClientUser
-			diff = statsAfter.ClientUser.BytesReceived.Sum - statsBefore.ClientUser.BytesReceived.Sum
-			if diff > 1.0 {
-				t.Errorf("Difference in ClientUser.BytesReceived.Sum is too large (> 1.0): %f", diff)
-			}
-			diff = statsAfter.ClientUser.BytesSent.Sum - statsBefore.ClientUser.BytesSent.Sum
-			if diff > 1.0 {
-				t.Errorf("Difference in ClientUser.BytesSent.Sum is too large (> 1.0): %f", diff)
-			}
-			var intdiff int64
-			intdiff = statsAfter.ClientUser.BytesReceived.Count - statsBefore.ClientUser.BytesReceived.Count
-			if intdiff > 0 {
-				t.Errorf("Difference in ClientUser.BytesReceived.Count is too large (> 0): %d", intdiff)
-			}
-			intdiff = statsAfter.ClientUser.BytesSent.Count - statsBefore.ClientUser.BytesSent.Count
-			if intdiff > 0 {
-				t.Errorf("Difference in ClientUser.BytesSent.Count is too large (> 0): %d", intdiff)
-			}
+	    // Traffic is superuser, so nothing should be counted in ClientUser,
+			// not even the statistics calls.
+			checkTrafficAtMost(t, &statsBefore, &statsAfter, user,
+												 &limits{Sent: 0.1, Received: 0.1,
+												         SentCount: 0, RecCount: 0});
 		} else {
 			t.Logf("Authentication %s is not jwt superuser, expecting to see user traffic...", auth)
 			// Traffic is either unauthenticated or with password, so there should
 			// be traffic in ClientUser
-			diff = statsAfter.ClientUser.BytesReceived.Sum - statsBefore.ClientUser.BytesReceived.Sum
-			if diff < 40000.0 {
-				t.Errorf("Difference in ClientUser.BytesReceived.Sum is too small (< 40000.0): %f", diff)
-			}
-			diff = statsAfter.ClientUser.BytesSent.Sum - statsBefore.ClientUser.BytesSent.Sum
-			if diff < 100000.0 {
-				t.Errorf("Difference in ClientUser.BytesSent.Sum is too small (< 100000.0): %f", diff)
-			}
-			var intdiff int64
-			intdiff = statsAfter.ClientUser.BytesReceived.Count - statsBefore.ClientUser.BytesReceived.Count
-			if intdiff < 1000 {
-				t.Errorf("Difference in ClientUser.BytesReceived.Count is too small (< 1000): %d", intdiff)
-			}
-			intdiff = statsAfter.ClientUser.BytesSent.Count - statsBefore.ClientUser.BytesSent.Count
-			if intdiff < 1000 {
-				t.Errorf("Difference in ClientUser.BytesSent.Count is too small (< 1000): %d", intdiff)
-			}
+			checkTrafficAtLeast(t, &statsBefore, &statsAfter, user,
+												  &limits{Sent: 100000.0, Received: 40000.0,
+												          SentCount: 1000, RecCount: 1000});
 		}
 	} else {
 		t.Log("Skipping ClientUser tests for statistics, since API is not present.")
 	}
 }
+
+// myQueryRequest is used below for a special query test for forwarding.
+type myQueryRequest struct {
+  Query string  `json:"query"`
+	BatchSize int `json:"batchSize,omitempty"`
+}
+
+// cursorData is used to dig out the ID of the cursor
+type myCursorData struct {
+	ID      string `json:"id"`
+	HasMore bool   `json:"hasMore,omitempty"`
+}
+
+// TestServerStatisticsForwarding tests if Client.Statistics increase
+// with traffic in the correct way if queries are forwarded between
+// coordinators.
+func TestServerStatisticsForwarding(t *testing.T) {
+	c := createClientFromEnv(t, true)
+	ctx := context.Background()
+
+	_, err := c.Cluster(ctx)
+	if driver.IsPreconditionFailed(err) {
+		t.Skip("Not a cluster")
+	} else if err != nil {
+		t.Fatalf("Health failed: %s", describe(err))
+	}
+
+	checkEnabled(t, c, ctx)
+
+	conn := c.Connection()
+	endpoints := conn.Endpoints()
+
+	if len(endpoints) < 2 {
+		t.Fatalf("Did not have at least two endpoints. Giving up.")
+	}
+
+	// Do a preliminary test to see if we can do some traffic on one coordinator
+	// and not see it on the second one.
+
+	ctx1 := driver.WithEndpoint(context.Background(), endpoints[0])
+	ctx2 := driver.WithEndpoint(context.Background(), endpoints[1])
+
+	statsBefore, err := c.Statistics(ctx2)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+
+	// At least 5000 documents in the collection:
+	doSomeWrites(t, ctx1, c)
+	doSomeWrites(t, ctx1, c)
+	doSomeWrites(t, ctx1, c)
+	doSomeWrites(t, ctx1, c)
+	doSomeWrites(t, ctx1, c)
+
+	time.Sleep(time.Second)  // wait for statistics to settle
+
+	statsAfter, err := c.Statistics(ctx2)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+
+	// No traffic on second coordinator (besides statistics calls):
+  checkTrafficAtMost(t, &statsBefore, &statsAfter, all,
+	                   &limits{Received: 400, Sent: 4000,
+								             RecCount: 2, SentCount: 2})
+
+	// First ask for a cursor on coordinator 1:
+	req, err := conn.NewRequest("POST", "_db/statistics_test/_api/cursor")
+	if err != nil {
+		t.Fatalf("Error in NewRequest call for cursor: %s", describe(err))
+	}
+	query := myQueryRequest{
+	  Query: "FOR x IN statistics_test RETURN x",
+		BatchSize: 1000,
+	}
+	if _, err := req.SetBody(query); err != nil {
+		t.Fatalf("Error in SetBody call for cursor: %s", describe(err))
+	}
+	resp, err := conn.Do(ctx1, req)
+	if err != nil {
+		t.Fatalf("Error in Do call for cursor: %s", describe(err))
+	}
+	var cursorBody myCursorData
+	err = resp.ParseBody("", &cursorBody)
+	if err != nil || !cursorBody.HasMore {
+		t.Fatalf("Error in cursor call: %s", describe(err))
+	}
+
+	time.Sleep(time.Second)
+
+	statsBefore1, err := c.Statistics(ctx1)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+	statsBefore2, err := c.Statistics(ctx2)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+
+	// Now issue a cursor continuation call to the second coordinator:
+	req, err = conn.NewRequest("PUT", "_db/statistics_test/_api/cursor/" + cursorBody.ID)
+	if err != nil {
+		t.Fatalf("Error in NewRequest call for cursor cont: %s", describe(err))
+	}
+	_, err = conn.Do(ctx2, req)
+	if err != nil {
+		t.Fatalf("Error in Do call for cursor cont: %s", describe(err))
+	}
+
+	time.Sleep(time.Second)  // wait until statistics settled
+
+	statsAfter1, err := c.Statistics(ctx1)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+	statsAfter2, err := c.Statistics(ctx2)
+	if err != nil {
+		t.Fatalf("Error in statistics call: %s", describe(err))
+	}
+
+	// Second coordinator should not count as user traffic (besides maybe
+	// the statistics calls):
+	t.Logf("Checking user traffic on coordinator2...")
+	checkTrafficAtMost(t, &statsBefore2, &statsAfter2, user,
+										 &limits{Received: 400, Sent: 4000,
+														 RecCount: 2, SentCount: 2})
+	// However, first coordinator should have counted the user traffic,
+	// note: it was just a single request with nearly no upload but quite
+	// some download:
+	t.Logf("Checking user traffic on coordinator1...")
+	checkTrafficAtLeast(t, &statsBefore1, &statsAfter1, user,
+										  &limits{Received: 0, Sent: 40000,
+														  RecCount: 1, SentCount: 1})
+}
+
