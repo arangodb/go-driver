55b2fd6bc39e390dc3a2f9fb2b9c736eafbf3925
ajanikow <adam.janikowski@ce-solutions.pl>
2019-12-25 11:32:51 +0000

    Fix test

--- test/database_collection_defaults_test.go
+++ test/database_collection_defaults_test.go
@@ -23,7 +23,7 @@
 package test
 
 import (
-	"github.com/arangodb/kube-arangodb/.gobuild/pkg/mod/github.com/dchest/uniuri@v0.0.0-20160212164326-8902c56451e9"
+	"github.com/dchest/uniuri"
 	"strings"
 	"testing"
 
