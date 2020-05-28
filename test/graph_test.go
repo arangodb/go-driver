f810f705ecc4c29075e51b747a5d6422a78770bb
Gergely Brautigam <gergely@arangodb.com>
2020-05-28 10:12:13 +0200

    Added test which works on 3.5

--- test/graph_test.go
+++ test/graph_test.go
@@ -154,6 +154,73 @@ func TestCreateGraphWithOptions(t *testing.T) {
 	}
 }
 
+func TestCreateGraphWithOptions35(t *testing.T) {
+	c := createClientFromEnv(t, true)
+	db := ensureDatabase(context.TODO(), c, "graph_test", nil, t)
+	name := "test_create_graph_2"
+	options := &driver.CreateGraphOptions{
+		OrphanVertexCollections: []string{"orphan1", "orphan2"},
+		EdgeDefinitions: []driver.EdgeDefinition{
+			{
+				Collection: "coll",
+				To:         []string{"to-coll1"},
+				From:       []string{"from-coll1"},
+			},
+		},
+		NumberOfShards:    2,
+		ReplicationFactor: 3,
+	}
+	if _, err := db.CreateGraph(context.TODO(), name, options); err != nil {
+		t.Fatalf("Failed to create graph '%s': %s", name, describe(err))
+	}
+	// Graph must exist now
+	if found, err := db.GraphExists(context.TODO(), name); err != nil {
+		t.Errorf("GraphExists('%s') failed: %s", name, describe(err))
+	} else if !found {
+		t.Errorf("GraphExists('%s') return false, expected true", name)
+	}
+	// Graph must be listed
+	if list, err := db.Graphs(context.TODO()); err != nil {
+		t.Errorf("Graphs failed: %s", describe(err))
+	} else {
+		found := false
+		for _, g := range list {
+			if g.Name() == name {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Graph '%s' not found in list", name)
+		}
+	}
+	// Open graph
+	g, err := db.Graph(context.TODO(), name)
+	if err != nil {
+		t.Errorf("Graph('%s') failed: %s", name, describe(err))
+	} else if g.Name() != name {
+		t.Errorf("Graph.Name wrong. Expected '%s', got '%s'", name, g.Name())
+	}
+	if g.NumberOfShards() != options.NumberOfShards {
+		t.Errorf("Graph.NumberOfShards wrong. Expected '%d', got '%d'", options.NumberOfShards, g.NumberOfShards())
+	}
+	if g.ReplicationFactor() != options.ReplicationFactor {
+		t.Errorf("Graph.ReplicationFactor wrong. Expected '%d', got '%d'", options.ReplicationFactor, g.ReplicationFactor())
+	}
+	if g.EdgeDefinitions()[0].Collection != options.EdgeDefinitions[0].Collection {
+		t.Errorf("Graph.EdgeDefinitions.collection wrong. Expected '%s', got '%s'", options.EdgeDefinitions[0].Collection, g.EdgeDefinitions()[0].Collection)
+	}
+	if g.EdgeDefinitions()[0].From[0] != options.EdgeDefinitions[0].From[0] {
+		t.Errorf("Graph.EdgeDefinitions.from wrong. Expected '%s', got '%s'", options.EdgeDefinitions[0].From[0], g.EdgeDefinitions()[0].From[0])
+	}
+	if g.EdgeDefinitions()[0].To[0] != options.EdgeDefinitions[0].To[0] {
+		t.Errorf("Graph.EdgeDefinitions.to wrong. Expected '%s', got '%s'", options.EdgeDefinitions[0].To[0], g.EdgeDefinitions()[0].To[0])
+	}
+	if g.OrphanCollections()[0] != options.OrphanVertexCollections[0] && g.OrphanCollections()[1] != options.OrphanVertexCollections[1] {
+		t.Errorf("Graph.IsSmart wrong. Expected '%v', got '%v'", options.OrphanVertexCollections, g.OrphanCollections())
+	}
+}
+
 // TestRemoveGraph creates a graph and then removes it.
 func TestRemoveGraph(t *testing.T) {
 	c := createClientFromEnv(t, true)
