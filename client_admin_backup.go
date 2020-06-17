3a9b518b5892ebd2257d7640075d7a3c5181c984
ajanikow <12255597+ajanikow@users.noreply.github.com>
2020-06-17 11:20:03 +0000

    Fix JSON

--- client_admin_backup.go
+++ client_admin_backup.go
@@ -38,7 +38,7 @@ type BackupMeta struct {
 	PotentiallyInconsistent bool                 `json:"potentiallyInconsistent,omitempty"`
 	Available               bool                 `json:"available,omitempty"`
 	NumberOfPiecesPresent   uint                 `json:"nrPiecesPresent,omitempty"`
-	Keys                    BackupMetaSha256List `json:"keys.omitempty"`
+	Keys                    BackupMetaSha256List `json:"keys,omitempty"`
 }
 
 // BackupMetaSha256List baskup sha list
