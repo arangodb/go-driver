cdb2ca2b29cab33a5ebaf6eb4bd6ff82b1e045b3
Tomasz Mielech <informal.ict@gmail.com>
2021-08-23 07:27:58 +0200

    refactor connection.Do for better legibility

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,7 +1,7 @@
 //
 // DISCLAIMER
 //
-// Copyright 2017 ArangoDB GmbH, Cologne, Germany
+// Copyright 2017-2021 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@
 // Copyright holder is ArangoDB GmbH, Cologne, Germany
 //
 // Author Ewout Prangsma
+// Author Tomasz Mielech
 //
 
 package cluster
@@ -31,7 +32,7 @@ import (
 	"sync"
 	"time"
 
-	driver "github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver"
 )
 
 const (
@@ -105,24 +106,15 @@ func (c *clusterConnection) NewRequest(method, path string) (driver.Request, err
 
 // Do performs a given request, returning its response.
 func (c *clusterConnection) Do(ctx context.Context, req driver.Request) (driver.Response, error) {
-	followLeaderRedirect := true
+	var timeout time.Duration
+
 	if ctx == nil {
 		ctx = context.Background()
-	} else {
-		if v := ctx.Value(keyFollowLeaderRedirect); v != nil {
-			if on, ok := v.(bool); ok {
-				followLeaderRedirect = on
-			}
-		}
-	}
-	// Timeout management.
-	// We take the given timeout and divide it in 3 so we allow for other servers
-	// to give it a try if an earlier server fails.
-	deadline, hasDeadline := ctx.Deadline()
-	var timeout time.Duration
-	if hasDeadline {
+	} else if deadline, hasDeadline := ctx.Deadline(); hasDeadline {
 		timeout = deadline.Sub(time.Now())
-	} else {
+	}
+
+	if timeout == 0 {
 		timeout = c.defaultTimeout
 	}
 
@@ -149,54 +141,52 @@ func (c *clusterConnection) Do(ctx context.Context, req driver.Request) (driver.
 
 	attempt := 1
 	for {
-		// Send request to specific endpoint with a 1/3 timeout (so we get 3 attempts)
+		// Send request to specific endpoint with a 1/[1,2,3] timeout (so we get 1 or 2 or 3 attempts)
 		serverCtx, cancel := context.WithTimeout(ctx, durationPerRequest)
 		resp, err := server.Do(serverCtx, req)
 		cancel()
 
-		isNoLeaderResponse := false
-		if err == nil && resp.StatusCode() == 503 {
-			// Service unavailable, parse the body, perhaps this is a "no leader"
-			// case where we have to failover.
-			var aerr driver.ArangoError
-			if perr := resp.ParseBody("", &aerr); perr == nil && aerr.HasError {
-				if driver.IsNoLeader(aerr) {
-					isNoLeaderResponse = true
-					// Save error in case we have no more servers
-					err = aerr
+		if err == nil && resp.StatusCode() == http.StatusServiceUnavailable && c.canFollowLeader(ctx) {
+			// Service unavailable, handle the "no leader" error.
+			var errArango driver.ArangoError
+
+			if resp.ParseBody("", &errArango) == nil && errArango.HasError && driver.IsNoLeader(errArango) {
+				attempt++
+				if attempt > serverCount {
+					// A specific server was specified or all servers failed. Give up.
+					return nil, driver.WithStack(errArango)
 				}
+				server = c.getNextServer()
+				continue
 			}
 		}
 
-		if !isNoLeaderResponse || !followLeaderRedirect {
-			if err == nil {
-				// We're done
-				return resp, nil
-			}
-			// No success yet
-			if driver.IsCanceled(err) {
-				// Request was cancelled, we return directly.
+		if err == nil {
+			// The response has been received.
+			return resp, nil
+		}
+
+		// No success yet.
+		if driver.IsCanceled(err) {
+			// Request was cancelled, we return directly.
+			return nil, driver.WithStack(err)
+		}
+
+		if req.Written() {
+			// Request has been written to network, do not failover. The error must be returned.
+			if driver.IsArangoError(err) {
+				// ArangoError, so we got an error response from server.
 				return nil, driver.WithStack(err)
 			}
-			// If we've completely written the request, we return the error,
-			// otherwise we'll failover to a new server.
-			if req.Written() {
-				// Request has been written to network, do not failover
-				if driver.IsArangoError(err) {
-					// ArangoError, so we got an error response from server.
-					return nil, driver.WithStack(err)
-				}
-				// Not an ArangoError, so it must be some kind of timeout, network ... error.
-				return nil, driver.WithStack(&driver.ResponseError{Err: err})
-			}
+
+			// Not an ArangoError, so it must be some kind of timeout, network ... error.
+			return nil, driver.WithStack(&driver.ResponseError{Err: err})
 		}
 
-		// Failed, try next server
+		// The request has not been written, try next server.
 		attempt++
 		if attempt > serverCount {
-			// A specific server was specified, no failover.
-			// or
-			// We've tried all servers. Giving up.
+			// A specific server was specified or all servers failed. Give up.
 			return nil, driver.WithStack(err)
 		}
 		server = c.getNextServer()
@@ -353,4 +343,15 @@ func (c *clusterConnection) getNextServer() driver.Connection {
 	defer c.mutex.Unlock()
 	c.current = (c.current + 1) % len(c.servers)
 	return c.servers[c.current]
+}
+
+// canFollowLeader returns true if searching leader is enabled.
+func (c *clusterConnection) canFollowLeader(ctx context.Context) bool {
+	if v := ctx.Value(keyFollowLeaderRedirect); v != nil {
+		if on, ok := v.(bool); ok && !on {
+			return false
+		}
+	}
+
+	return true
 }
