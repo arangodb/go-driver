81d59d5ac7f01dd1631725c82f656a51d932f277
jwierzbo <jakub.wierzbowski@arangodb.com>
2023-04-03 14:04:47 +0200

    Retry behaviour in case of not fulfilled write concern

--- test/cluster.sh
+++ test/cluster.sh
@@ -55,7 +55,7 @@ if [ "$CMD" == "start" ]; then
         DOCKERPLATFORMARG="--platform linux/x86_64"
         DOCKERARGS="$DOCKERARGS $DOCKERPLATFORMARG"
     fi
-    STARTERARGS="$STARTERARGS --docker.container=$STARTERCONTAINER"
+    STARTERARGS="$STARTERARGS --docker.container=$STARTERCONTAINER --cluster.failed-write-concern-error-code=503"
 
     if [ -z "$STARTERPORT" ]; then
         STARTERPORT=7000
