ade9ded418341c2bc3cd4741f3766c9cf3820902
Wilfried Goesgens <willi@arangodb.com>
2025-06-27 08:50:56 +0200

    test

--- v2/tests/utils_retry_test.go
+++ v2/tests/utils_retry_test.go
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//  Copyright holder is ArangoDB GmbH, Cologne, Germany
 //
 
 package tests
