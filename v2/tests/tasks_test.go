81774348fe59ba6ff5ac247dbe48734c348acb77
root <root@DESKTOP-MN1K3O5.localdomain>
2025-07-09 21:02:49 +0530

    add the licence in the file

--- v2/tests/tasks_test.go
+++ v2/tests/tasks_test.go
@@ -1,3 +1,23 @@
+//
+// DISCLAIMER
+//
+// Copyright 2024 ArangoDB GmbH, Cologne, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright holder is ArangoDB GmbH, Cologne, Germany
+//
+
 package tests
 
 import (
@@ -35,12 +55,12 @@ func Test_TaskCreation(t *testing.T) {
 
 		for name, options := range testCases {
 			withContextT(t, defaultTestTimeout, func(ctx context.Context, tb testing.TB) {
-				createtdTask, err := client.CreateTask(ctx, options)
+				createdTask, err := client.CreateTask(ctx, options)
 				require.NoError(t, err)
-				require.NotNil(t, createtdTask)
-				require.Equal(t, name, createtdTask.Name())
+				require.NotNil(t, createdTask)
+				require.Equal(t, name, createdTask.Name())
 
-				taskInfo, err := client.Task(ctx, createtdTask.ID())
+				taskInfo, err := client.Task(ctx, createdTask.ID())
 				require.NoError(t, err)
 				require.NotNil(t, taskInfo)
 				require.Equal(t, name, taskInfo.Name())
@@ -52,8 +72,8 @@ func Test_TaskCreation(t *testing.T) {
 				t.Logf("Found tasks: %v", tasks)
 				fmt.Printf("Number of tasks: %s\n", tasks[0].ID())
 
-				require.NoError(t, client.RemoveTask(ctx, createtdTask.ID()))
-				t.Logf("Task %s removed successfully", createtdTask.ID())
+				require.NoError(t, client.RemoveTask(ctx, createdTask.ID()))
+				t.Logf("Task %s removed successfully", createdTask.ID())
 			})
 		}
 	}, WrapOptions{
