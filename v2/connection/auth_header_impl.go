3f24000b97d47b945fc0f39fde504df6e4afd8e6
jwierzbo <jakub.wierzbowski@arangodb.com>
2025-11-06 10:55:20 +0100

    Update for Go 1.24 and dependency upgrades

--- v2/connection/auth_header_impl.go
+++ v2/connection/auth_header_impl.go
@@ -1,7 +1,7 @@
 //
 // DISCLAIMER
 //
-// Copyright 2020 ArangoDB GmbH, Cologne, Germany
+// Copyright 2020-2025 ArangoDB GmbH, Cologne, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@
 //
 // Copyright holder is ArangoDB GmbH, Cologne, Germany
 //
-// Author Adam Janikowski
-//
 
 package connection
 
@@ -28,9 +26,13 @@ import (
 )
 
 func NewHeaderAuth(key, value string, args ...interface{}) Authentication {
+	if len(args) > 0 {
+		value = fmt.Sprintf(value, args...)
+	}
+
 	return &headerAuth{
 		key:   key,
-		value: fmt.Sprintf(value, args...),
+		value: value,
 	}
 }
 
